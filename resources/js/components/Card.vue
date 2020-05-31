072736d4eb9ded04a101e07a7bf50df464230185
QuanDo <thienkimlove@gmail.com>
2020-06-01 01:25:07 +0700

    update

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,8 +1,17 @@
 <template>
     <card class="flex flex-col items-center justify-center">
-        <div class="px-3 py-3 w-full">
-            <h1 class="text-center text-3xl text-80 font-light">Chào mừng đến hệ thống quản lý giao dịch.</h1>
+
+
+        <div class="card">
+            <div class="flex items-center py-3 border-b border-50">
+                <div class="flex items-center">
+                    <h2 class="text-center text-3xl text-40 font-light">Chào mừng đến hệ thống quản lý giao dịch.</h2>
+                </div>
+
+                 <div class="overflow-hidden overflow-x-auto relative"><!----></div>
+            </div>
         </div>
+
     </card>
 </template>
 
